1e8eaf98d1392198cd1d50613abdb181b9ecf8d3
c6 <31777460+c6-dev@users.noreply.github.com>
2024-02-27 13:48:38 +0400

    add update timer, red color for negative temp, text outline

--- TempOnHUDMain.cs
+++ TempOnHUDMain.cs
@@ -12,6 +12,8 @@ internal sealed class TempOnHUDMain : MelonMod
     [HarmonyPatch(typeof(StatusBar), "Update")]
     private static class TempMeter
     {
+        private static double elapsedMinutes = 0d;
+        private static Color darkRed = new Color(0.8f, 0.2f, 0.23f, 1.000f);
         private static void Postfix(StatusBar __instance)
         {
             if (__instance.m_StatusBarType != StatusBar.StatusBarType.Hunger) return;
@@ -20,31 +22,44 @@ internal sealed class TempOnHUDMain : MelonMod
             UISprite sprite = __instance.m_OuterBoxSprite.GetComponent<UISprite>();
             GameObject spriteObject = sprite.gameObject;
             GameObject tempObject = spriteObject.transform.parent.FindChild("temperature")?.gameObject;
+
             if (tempObject == null)
             {
                 // init
                 tempObject = new GameObject("temperature");
                 tempObject.transform.SetParent(spriteObject.transform.parent);
                 tempObject.transform.localScale = spriteObject.transform.localScale;
-                tempObject.transform.localPosition = new Vector3(sprite.width + 15, 0, 0);
+                tempObject.transform.localPosition = new Vector3(sprite.width + 17, 0, 0);
 
                 UILabel tempLabel = tempObject.AddComponent<UILabel>();
                 tempLabel.text = "0°C";
                 tempLabel.color = Color.white;
                 tempLabel.fontStyle = FontStyle.Normal;
                 tempLabel.font = GameManager.GetFontManager().GetUIFontForCharacterSet(CharacterSet.Latin);
-                tempLabel.fontSize = 24;
-                tempLabel.width = sprite.width;
-                tempLabel.height = sprite.height;
-            } else
+                tempLabel.fontSize = 32;
+                tempLabel.effectStyle = UILabel.Effect.Outline;
+                tempLabel.effectColor = new Color(0.125f, 0.094f, 0.094f, 0.6f);
+                tempLabel.effectDistance = new Vector2(1.7f, 1.7f);
+
+            }
+            else if (GameManager.GetHighResolutionTimerManager().GetElapsedMinutes() - elapsedMinutes >= 0.1d)
             {
-                // update
+                // update every 0.1 ingame minutes
                 UILabel tempLabel = tempObject.GetComponent<UILabel>();
-                if (tempLabel)
+                if (tempLabel != null && GameManager.GetFreezingComponent() != null)
                 {
 
                     int temp = (int)Math.Round(GameManager.GetFreezingComponent().CalculateBodyTemperature());
-                    tempLabel.text = temp.ToString() + "°C"; ;
+                    if (temp <= 0)
+                    {
+                        tempLabel.color = darkRed;
+                    }
+                    else
+                    {
+                        tempLabel.color = Color.white;
+                    }
+                    tempLabel.text = temp.ToString() + "°C";
+                    elapsedMinutes = GameManager.GetHighResolutionTimerManager().GetElapsedMinutes();
                 }
             }
 
